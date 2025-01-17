ef81da3e288d5a49f5ac7b2289c4157079827446
kreesstore <manueldrs2006@gmail.com>
2025-01-17 13:24:19 -0500

    Add files via upload

---
+++ JS/codigo_index.js
@@ -0,0 +1,191 @@
+function redirirgir(){
+    event.preventDefault();
+    window.location.href = "HTML/tienda.html"
+}
+
+class Producto {
+    constructor(id, nombre, precio_mlc, precio_cup, marca, categoria, cantidad, imagen = null) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio_mlc = precio_mlc;
+        this.precio_cup = precio_cup;
+        this.marca = marca;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+        this.imagen = imagen;
+    }
+}
+
+// Lista de Ultimos Productos Agregados //
+let productosTotales = [
+
+    new Producto(1, "Bateria 12V 75AH", "145.00$", "57 384.40$", "No Registrada", "baterias", 0, "IMG/Productos/Automotriz/bateria/75AH.jpg"),
+    
+    new Producto(1002, "Correa", "$", "3 537.57$", "L2-20", "correas", 0, "IMG/Productos/Alt/correa.jpg"),
+
+    new Producto(1101, "Disco de Freno", "$", "9 402.03$", "MITSUBISHI MONTERO/L200 4WD", "discos", 0, "IMG/Productos/Alt/disco.jpg"),
+
+    new Producto(301, "Amortiguador Derecho", "77.64$", "21 292.07$", "Atos", "amortiguacion", 1, "IMG/Productos/Alt/amortigua.png"),
+
+
+];
+
+// Obtener elementos del DOM
+let contenedorResultados = document.getElementById("contenedor_resultados");
+
+function mostrarProductos(productos) {
+    contenedorResultados.innerHTML = ""; // Limpiar resultados anteriores
+
+    productos.forEach(producto => {
+        let cajaContenedora = document.createElement("div");
+        cajaContenedora.classList.add("producto-caja");
+        let cajaSecundaria = document.createElement("div");
+        cajaSecundaria.classList.add("caja-secundaria");
+        // Mostrar solo el nombre, la marca y el precio en la vista inicial
+        cajaSecundaria.innerHTML = `<p><span class="producto_span">Nombre:</span> ${producto.nombre}</p>
+                                    <p><span class="producto_span">Marca:</span> ${producto.marca}</p>`;
+        cajaContenedora.appendChild(cajaSecundaria)
+
+        // Verificar si el producto tiene una imagen y establecerla como fondo
+        if (producto.imagen) {
+            cajaContenedora.style.backgroundImage = `url(${producto.imagen})`;
+            cajaContenedora.style.backgroundSize = "cover";  // Para ajustar la imagen al contenedor
+            cajaContenedora.style.backgroundPosition = "center center"; // Centrar la imagen
+        }
+
+        if (this.cantidad == 0) {
+            console.log("Ya estamos en 0")
+        }
+
+        // Agregar evento para expandir el producto cuando se hace clic
+        cajaContenedora.addEventListener("click", () => abrirProductoEmergente(producto));
+
+        contenedorResultados.appendChild(cajaContenedora);
+    });
+}
+
+function abrirProductoEmergente(producto) {
+    // Crear un contenedor de fondo para el "popup"
+    let fondoPopup = document.createElement("div");
+    fondoPopup.classList.add("fondo-popup");
+
+    // Crear el contenedor del producto ampliado
+    let productoAmpliado = document.createElement("div");
+    productoAmpliado.classList.add("producto-popup");
+
+    // Rellenar con toda la información del producto
+    let contenidoProducto = `
+        <p><span class="producto_span">ID:</span> ${producto.id}</p>
+        <p><span class="producto_span">Nombre:</span> ${producto.nombre}</p>
+        <p><span class="producto_span">Precio MLC:</span> ${producto.precio_mlc}</p>
+        <p><span class="producto_span">Precio CUP:</span> ${producto.precio_cup}</p>
+        <p><span class="producto_span">Marca:</span> ${producto.marca}</p>
+        <p><span class="producto_span">Cantidad Disponible:</span> ${producto.cantidad}</p>`;
+
+    // Verificar si hay una imagen para mostrar
+    if (producto.imagen) {
+        contenidoProducto = `<img src="${producto.imagen}" alt="${producto.nombre}" class="imagen-producto">` + contenidoProducto;
+    }
+
+    // Añadir contenido al contenedor del producto ampliado
+    productoAmpliado.innerHTML = contenidoProducto;
+
+    // Botón para cerrar el "popup"
+    let botonCerrar = document.createElement("button");
+    botonCerrar.textContent = "Cerrar";
+    botonCerrar.classList.add("boton-cerrar");
+    botonCerrar.addEventListener("click", () => {
+        document.body.removeChild(fondoPopup); // Elimina el popup
+    });
+
+    productoAmpliado.appendChild(botonCerrar);
+    fondoPopup.appendChild(productoAmpliado);
+    document.body.appendChild(fondoPopup); // Añadir el fondo del popup a la página
+}
+
+// Filtrar productos por categoría
+function filtrarPorCategoria(categoria) {
+    let productosFiltrados = productosTotales.filter(producto => producto.categoria === categoria);
+    mostrarProductos(productosFiltrados);
+}
+
+// Búsqueda de productos
+function buscarProducto(event) {
+    event.preventDefault(); // Prevenir recarga de la página
+
+    let valorBusqueda = document.getElementById("input_busqueda").value.toLowerCase().trim();
+    if (valorBusqueda.length < 3) {
+        alert("Debe ingresar al menos 3 caracteres para la búsqueda.");
+        return;
+    }
+
+    let productosFiltrados = productosTotales.filter(producto =>
+        producto.nombre.toLowerCase().includes(valorBusqueda) ||
+        producto.marca.toLowerCase().includes(valorBusqueda)
+    );
+
+    if (productosFiltrados.length > 0) {
+        mostrarProductos(productosFiltrados);
+    } else {
+        contenedorResultados.innerHTML = "<p>No se encontraron productos con ese nombre.</p>";
+    }
+}
+
+mostrarProductos(productosTotales)
+
+
+
+
+
+
+
+
+
+
+
+
+function abrirProductoEmergenteOferta() {
+    // Crear un contenedor de fondo para el "popup"
+    let fondoPopup = document.createElement("div");
+    fondoPopup.classList.add("fondo-popup");
+
+    // Crear el contenedor del producto ampliado
+    let productoAmpliado = document.createElement("div");
+    productoAmpliado.classList.add("producto-popup");
+
+    let producto = new Producto(201, "Neumático Delantero 500*8", "$", "14 346.41$", "No Registrado", "neumaticos", 0, "IMG/Automotriz/neumatico.jpg")
+
+    // Rellenar con toda la información del producto
+    let contenidoProducto = `
+        <p><span class="producto_span">ID:</span> ${producto.id}</p>
+        <p><span class="producto_span">Nombre:</span> ${producto.nombre}</p>
+        <p><span class="producto_span">Precio MLC:</span> ${producto.precio_mlc}</p>
+        <p><span class="producto_span">Precio CUP:</span> ${producto.precio_cup} | <span class="oferta_span">15 000.00$</span></p>
+        <p><span class="producto_span">Marca:</span> ${producto.marca}</p>
+        <p><span class="producto_span">Cantidad Disponible:</span> ${producto.cantidad}</p>`;
+
+
+    // Verificar si hay una imagen para mostrar
+    if (producto.imagen) {
+        contenidoProducto = `<img src="${producto.imagen}" alt="${producto.nombre}" class="imagen-producto">` + contenidoProducto;
+    }
+
+    // Añadir contenido al contenedor del producto ampliado
+    productoAmpliado.innerHTML = contenidoProducto;
+
+    // Botón para cerrar el "popup"
+    let botonCerrar = document.createElement("button");
+    botonCerrar.textContent = "Cerrar";
+    botonCerrar.classList.add("boton-cerrar");
+    botonCerrar.addEventListener("click", () => {
+        document.body.removeChild(fondoPopup); // Elimina el popup
+    });
+
+    productoAmpliado.appendChild(botonCerrar);
+    fondoPopup.appendChild(productoAmpliado);
+    document.body.appendChild(fondoPopup); // Añadir el fondo del popup a la página
+}
+
+let oferta_avierta = document.getElementById("oferta_avierta_ID")
+let oferta = document.getElementById("oferta");
+oferta.addEventListener("click", abrirProductoEmergenteOferta)
